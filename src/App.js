add523394d3e9867b7574fc2bd04095480cc0ea1
spellsharp <svcodin22@gmail.com>
2024-02-16 15:44:43 +0530

    Removed useless imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Calendar from "react-calendar";
 
 function App() {
   return (
--- src/components/MyCalendar.jsx
+++ src/components/MyCalendar.jsx
@@ -1,19 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import styled from "styled-components";
 
-function MyCalendar() {
+function MyCalendar({ onDateChange }) {
   const [date, setDate] = useState(new Date());
-
   const onChange = (newDate) => {
+    const isoDate = newDate.toISOString();
     setDate(newDate);
-    console.log(newDate);
+    onDateChange(isoDate);
   };
+  useEffect(() => {
+    onDateChange(date);
+  }, [date]);
+
+  const CalendarContainer = styled.div`
+    .react-calendar {
+      width: 350px;
+      max-width: 100%;
+      background: rgb(139 92 246);
+      border: 1px solid rgb(0, 0, 0);
+      font-family: Arial, Helvetica, sans-serif;
+      line-height: 1.125em;
+    }
+
+    .react-calendar__navigation button {
+      min-width: 44px;
+      background-color: rgb(139 92 246);
+    }
+
+    .react-calendar__navigation button:hover {
+      background-color: rgba(124, 58, 237, 0.233);
+    }
+
+    .react-calendar__month-view__weekdays {
+      text-align: center;
+      text-transform: uppercase;
+      font: inherit;
+      font-size: 0.75em;
+      font-weight: bold;
+      background-color: rgb(139 92 246);
+    }
+
+    /* Navigation styles */
+    .react-calendar__navigation {
+      display: flex;
+      justify-content: space-between;
+      color: #ffffff;
+    }
+
+    /* Tile styles */
+    .react-calendar__tile {
+      cursor: pointer;
+      background-color: rgb(139 92 246);
+      /* Other styles... */
+    }
+
+    /* Active day styles */
+    .react-calendar__tile--active {
+      background-color: rgba(124, 58, 237, 1);
+      color: white;
+      /* Other styles... */
+    }
+
+    .react-calendar__tile:hover {
+      // background-color: #2e2e2e;
+      background-color: rgba(124, 58, 237, 0.5);
+    }
+
+    .react-calendar--selectRange .react-calendar__tile--hover {
+      background-color: #e6e6e6;
+    }
+
+    /* Other custom styles... */
+  `;
 
   return (
-    <div className="bg-black">
+    <CalendarContainer>
       <Calendar onChange={onChange} value={date} />
-    </div>
+    </CalendarContainer>
   );
 }
 
